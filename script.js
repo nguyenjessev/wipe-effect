cd352261d2c3aab8e84f0a845cf0fd3c1fd0aabd
Jesse Nguyen <nguyenjessev@gmail.com>
2022-12-06 18:04:43 -0500

    Add scrolling in both directions with parallax/overlap/reveal effect

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+const numTiles = document.getElementsByClassName('img-container').length;
+
 document.getElementById('left-arrow').addEventListener('click', (e) => {
   const activeTile = document.querySelector('[data-status="active"');
+  const currentIndex = parseInt(activeTile.dataset.index);
+  const nextIndex = currentIndex < (numTiles - 1) ? (currentIndex + 1) : 0;
+  const rightTile = document.querySelector(`[data-index="${nextIndex}"]`);
 
-  activeTile.style.transform = "translate(-100%, 0)";
+  activeTile.dataset.status = 'left';
+  rightTile.dataset.status = 'active';
+
+  const nextRightIndex = nextIndex < (numTiles - 1) ? (nextIndex + 1) : 0;
+  const nextRightTile = document.querySelector(`[data-index="${nextRightIndex}"]`);
+
+  nextRightTile.dataset.status = 'becoming-right';
+});
+
+document.getElementById('right-arrow').addEventListener('click', (e) => {
+  const activeTile = document.querySelector('[data-status="active"');
+  const currentIndex = parseInt(activeTile.dataset.index);
+  const nextIndex = currentIndex > 0 ? (currentIndex - 1) : (numTiles - 1);
+  const leftTile = document.querySelector(`[data-index="${nextIndex}"]`);
+
+  activeTile.dataset.status = 'right';
+  leftTile.dataset.status = 'active';
+
+  const nextLeftIndex = nextIndex > 0 ? (nextIndex - 1) : (numTiles - 1);
+  const nextLeftTile = document.querySelector(`[data-index="${nextLeftIndex}"]`);
+
+  nextLeftTile.dataset.status = 'becoming-left';
 });
